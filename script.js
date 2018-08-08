34da149aa9366a47513e0774f3587589c25c726f
Vitaliy Andriiv <vit.djason@gmail.com>
2018-08-09 00:30:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+	let box = document.getElementById('box')
+	let  submit = document.getElementById('button');
+	//box.style. = '1120px';
+	box.style.padding = '50px','50px','50px','50px';
+	//box.style. = '1120px'}
+	}
+function fun4(){
+	let box = document.getElementById('box')
+	let  button4 = document.getElementById('button4');
+	//box.style. = '1120px';
+	box.style.padding = '150px','150px','150px','150px';
+	//box.style. = '1120px'}
+	}
+function fun5(){
+	let box = document.getElementById('box')
+	let  button8 = document.getElementById('button8');
+	//box.style. = '1120px';
+	box.style.padding = '200px','200px','200px','200px';
+	//box.style. = '1120px'}
+	}
+function fun6(){
+
+	let box = document.getElementById('box')
+	let  button16 = document.getElementById('button16');
+	//box.style. = '1120px';
+	box.style.padding = '250px','250px','250px','250px';
+	//box.style. = '1120px'}
+	}
+function fun3(){
+	let box = document.getElementById('box')
+	let  sub = document.getElementById('button2');
+	box.style.padding = '1px','1px','1px ','1px';
+}
+fun2()
+function fun() {
+			let range = document.getElementById('r1')
+			let paragraf = document.getElementById('paragraf')
+			paragraf.innerHTML = range.value;
+}
